bd1a79dd34f4dd80adae820417a92529ca951543
Marye10 <marianaecpe10@gmail.com>
2022-11-08 18:28:10 -0500

    Pruebas unitarias
    Creación de las pruebas unitarias del patrón fachada

---
+++ Fachada/PruebaFachadaTests/EntidadCrediticiaTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fachada;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprobacionCredito.Tests
+{
+    [TestClass()]
+    public class EntidadCrediticiaTests
+    {
+        [TestMethod()]
+        public void ObtieneResultadoValidacionTest()
+        {
+            //Arrange
+            Cliente[] pruebaClientes =
+            {
+                new Cliente() {Nombre = "Misty", PuntajeDataCredito = 100, ValorPrestamo = 20000},
+                new Cliente() {Nombre = "Donn", PuntajeDataCredito = 76, ValorPrestamo = 20000},
+                new Cliente() {Nombre = "Brook", PuntajeDataCredito = 73, ValorPrestamo = 20000}
+            };
+
+            //Act
+            int aprobadosEnA = 1;
+            int eaprobadosEnB = 2;
+
+            //Assert
+            Assert.AreEqual(aprobadosEnA, eaprobadosEnB);
+
+
+        }
+
+
+      
+
+
+    }
+    
+}
